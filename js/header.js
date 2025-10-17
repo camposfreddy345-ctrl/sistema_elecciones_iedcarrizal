aee1aa46ea0d395fec93aa744723535eaecff5a7
camposfreddy345-ctrl <camposfreddy345@gmail.com>
2025-10-17 16:17:15 -0500

    proyecto

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
-document.querySelector ('.encabezado-principal').innerHTML = `
-    <h1>IED TECNICA DE CARRIZAL</h1>
+document.querySelector('.encabezado-principal').innerHTML = `
+    <h1>IED TECNICA CARRIZAL</h1>
     <nav>
-        <a href="index.html">Inicio</a>
+        <a href="../index.html">Inicio</a>
         <a href="../vistas/registrar_estudiante.html">Estudiantes</a>
         <a href="../vistas/contacto.html">Contacto</a>
         <a href="../vistas/login.html">Login</a>
